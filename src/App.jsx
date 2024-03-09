dc3e1364a702401b635ea4f3a94391442d63b71d
Marko <baghashvili.marko@gmail.com>
2024-03-09 17:13:10 +0400

    I implemented routes for the pages, including the display of each order from the URL. Additionally, I implemented uploading the order with error handling, allowing for immediate navigation to the relevant order in case of an error.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Home from "./ui/Home.jsx";
-import Menu from "./features/menu/Menu.jsx";
+import Error from "./ui/Error.jsx";
+import Menu, { loader as menuLoader } from "./features/menu/Menu.jsx";
 import Cart from "./features/Cart/Cart.jsx";
-import CreateOrder from "./features/order/CreateOrder.jsx";
-import Order from "./features/order/Order.jsx";
+import CreateOrder, {
+  action as CreateOrderAction,
+} from "./features/order/CreateOrder.jsx";
+import Order, { loader as orderLoader } from "./features/order/Order.jsx";
 import AppLayout from "./ui/AppLayout.jsx";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -18,13 +22,24 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
         element: <Cart />,
       },
-      { path: "/order/new", element: <CreateOrder /> },
-      { path: "/order/:orderId", element: <Order /> },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: CreateOrderAction,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
     ],
   },
 ]);
