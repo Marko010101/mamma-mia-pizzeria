81208cb5526e94679ba9f12f433649bcfefba80a
Marko <baghashvili.marko@gmail.com>
2024-03-10 22:56:46 +0400

    Styled Home page with Tailwind

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Marko</div>;
+  return <div className="hidden text-sm font-semibold md:block">Marko</div>;
 }
 
 export default Username;
