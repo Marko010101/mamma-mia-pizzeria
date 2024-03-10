58426ed6d8d809c8cba2146b5e2d9cc7aad9f751
Marko <baghashvili.marko@gmail.com>
2024-03-10 20:08:03 +0400

    Started working on styling with Tailwind css

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="text-sm font-semibold">Marko</div>;
+}
+
+export default Username;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder.jsx";
+import Username from "../features/user/Username.jsx";
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Mamma Mia Pizzeria Co.</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Mamma Mia Pizzeria Co.
+      </Link>
 
       <SearchOrder />
-      <p>Marko</p>
+      <Username />
     </header>
   );
 }
